9f5a087c88699f3f43e5f42215b5bb0af87d9dd6
Grégoire Pelizzardi <gregpeli@yahoo.fr>
2025-08-20 15:01:49 +0200

    maj

--- src/app/settings/privacy/page.jsx
+++ src/app/settings/privacy/page.jsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import './setting.css';
+import "./privacy.css";
 
 export default function PrivacyPage() {
 const pathname = usePathname();
